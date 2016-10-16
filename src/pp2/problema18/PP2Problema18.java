e47c618e7889a1e956a14905cbf8121b17c829e9
PC <PC@192.168.1.70>
2016-10-16 18:59:07 -0500

    18.	Realizar un programa que contenga una función que calcule el mínimo común múltiplo de tres números.

--- src/pp2/problema18/PP2Problema18.java
+++ src/pp2/problema18/PP2Problema18.java
@@ -47,18 +47,24 @@ public class PP2Problema18 {
             numeromenor=c;
         }
         while(numeromenor>=divisor){ //Mientras que el numero menor sea mayor o igual al divisor, //8>=2 VERDADERO
-            if(a%divisor==0 && b%divisor==0 && c%divisor==0){ //Si el residuo de la division de a entre el divisor es 0 //2%2==0 && 8%2==0
+            if(a%divisor==0 || b%divisor==0 || c%divisor==0){ //Si el residuo de la division de a entre el divisor es 0 //2%2==0 && 8%2==0
                                               //y si el residuo de la division de b entre el divisor es 0 //VERDADERO && VERDADERO = VERDADERO
-                a=a/divisor; //a sera igual a la division de a entre el divisor //a=2/2 a=1
-                b=b/divisor;//b sera igual a la division de a entre el divisor  //b=8/2 b=4
-                c=c/divisor;
+                if(a%divisor==0){
+                    a=a/divisor;
+                }
+                if(b%divisor==0){
+                    b=b/divisor;
+                }
+                if(c%divisor==0){
+                    c=c/divisor;
+                }
                 d=d*divisor;//c sera igual a la multiplicacion de c por le divisor //c=1*2 c=2
             }else{ //Sino se cumple el if a divisor se le sumara 1
                 divisor++;
             }
         }
         //Se imprime el maximo comun denominador //c=2
-        System.out.print("El maximo comun denominador es: " + c);
+        System.out.print("El maximo comun denominador es: " + d);
     }
     
 }
