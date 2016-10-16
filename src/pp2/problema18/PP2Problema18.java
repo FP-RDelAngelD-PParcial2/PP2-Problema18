de76c3b0ef9f67460c73f8150379578fa476eb8f
PC <PC@192.168.1.70>
2016-10-16 18:22:27 -0500

    18.	Realizar un programa que contenga una función que calcule el mínimo común múltiplo de tres números.

--- src/pp2/problema18/PP2Problema18.java
+++ src/pp2/problema18/PP2Problema18.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package pp2.problema18;
-
+import java.util.*;
 /**
  *
  * @author PC
@@ -15,7 +15,25 @@ public class PP2Problema18 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int n1, n2, n3;
+        n1=verificarNumero("numero 1 ");
+        n2=verificarNumero("numero 2 ");
+        n3=verificarNumero("numero 3 ");
         
+    }
+     public static int solicitarDatos(String d){ //Solicita datos
+        int miDato;
+        Scanner entradaEscaner = new Scanner(System.in);
+        System.out.print("Introduce el dato " + d);
+        miDato= entradaEscaner.nextInt();
+        return miDato;
+    }
+    public static int verificarNumero(String d){
+        int a; //Variable
+        do{ //Hacer que solicite datos y se guarden en a 
+        a = solicitarDatos(d);
+        }while(a<=0);//Mientras que a sea menor o igual que 0
+        return a; //Regresa el valor de a
     }
     
 }
