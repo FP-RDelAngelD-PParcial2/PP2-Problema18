d12338f152e1a643c192eec556669f38802a4da7
PC <PC@192.168.1.70>
2016-10-16 19:29:53 -0500

    18.	Realizar un programa que contenga una función que calcule el mínimo común múltiplo de tres números.

--- src/pp2/problema18/PP2Problema18.java
+++ src/pp2/problema18/PP2Problema18.java
@@ -39,31 +39,34 @@ public class PP2Problema18 {
         int numeromaximo, d, divisor; //Declaracion de variables
         d=1; //Debe ser 1 ya que si es 0 se elimina el divisor
         divisor=2; //Comienza en 2 ya que es lo minimo en lo que se pueden dividir (media)
-        if(b<a && c<a){ //Se necesita conocer el numero menor para saber en cuanto se puede dividir //8>4>6
-            numeromaximo=a; //Si a es mayor que b, b es el numero menor //FALSO
-        }else if(a<b && c<b){
-            numeromaximo=b; //Sino el numero menor es a //numeromenor=8
+        //a=4, b=6, c=2
+        if(b<a && c<a){ //Se necesita conocer el numero mayor //6<4 && 2<4
+            numeromaximo=a; //Si b es menor que a, y c es menor que a, a es el numero mayor //FALSO
+        }else if(a<b && c<b){//Si a es menor que b, y c es menor que b, b es el numero mayor 
+            numeromaximo=b; //4<6 && 2<6 //VERDADERO
         }else{
-            numeromaximo=c;
+            numeromaximo=c;//Sino el numero maximo es c 
         }
-        while(numeromaximo>=divisor){ //Mientras que el numero menor sea mayor o igual al divisor, //8>=2 VERDADERO
-            if(a%divisor==0 || b%divisor==0 || c%divisor==0){ //Si el residuo de la division de a entre el divisor es 0 //2%2==0 && 8%2==0
-                                              //y si el residuo de la division de b entre el divisor es 0 //VERDADERO && VERDADERO = VERDADERO
-                d=d*divisor;
-                if(a%divisor==0){
-                    a=a/divisor;
+        while(numeromaximo>=divisor){ //Mientras que el numero mayor sea mayor o igual al divisor, //6>=2 VERDADERO
+            if(a%divisor==0 || b%divisor==0 || c%divisor==0){ //Si el residuo de la division de a entre el divisor es igual a 0 o //4%2==0 VERDADERO
+                                                              //el residuo de la division de b entre el divisor es igual a 0 o //6%2==0 VERDADERO
+                                                              //el residuo de la division de c entre el divisor es igual a 0 o //2%2==0 VERDADERO
+                                                              //La condicion sera verdadera y //VERDADERO
+                d=d*divisor; //se multiplicara el valor de d por el divisor //d=1*2 d=2
+                if(a%divisor==0){ //Si el residuo de la division de a entre el divisor es igual a 0 //4%2==0 VERDADERO
+                    a=a/divisor;//a sera igual a la division de a entre el divisor //a=4/2 a=2
                 }
-                if(b%divisor==0){
-                    b=b/divisor;
+                if(b%divisor==0){ //Si el residuo de la division de b entre el divisor es igual a 0 //6%2==0 VERDADERO
+                    b=b/divisor;//b sera igual a la division de b entre el divisor //b=6/2 b=3
                 }
-                if(c%divisor==0){
-                    c=c/divisor;
+                if(c%divisor==0){//Si el residuo de la division de c entre el divisor es igual a 0 //2%2==0 VERDADERO
+                    c=c/divisor;//c sera igual a la division de c entre el divisor //c=2/2 c=1
                 }
             }else{ //Sino se cumple el if a divisor se le sumara 1
                 divisor++;
             }
         }
-        //Se imprime el minimo comun multiplo //
+        //Se imprime el minimo comun multiplo //d=12
         System.out.print("El minimo comun multiplo es: " + d);
     }
     
