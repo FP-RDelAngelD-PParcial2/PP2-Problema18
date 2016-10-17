d5346eb7b82d8c61d317c2f2e3fab229e80c089e
PC <PC@192.168.1.70>
2016-10-16 19:09:16 -0500

    18.	Realizar un programa que contenga una función que calcule el mínimo común múltiplo de tres números.

--- src/pp2/problema18/PP2Problema18.java
+++ src/pp2/problema18/PP2Problema18.java
@@ -36,19 +36,20 @@ public class PP2Problema18 {
         return a; //Regresa el valor de a
     }
     public static void minimoComunD(int a, int b, int c){
-        int numeromenor, d, divisor; //Declaracion de variables
+        int numeromaximo, d, divisor; //Declaracion de variables
         d=1; //Debe ser 1 ya que si es 0 se elimina el divisor
         divisor=2; //Comienza en 2 ya que es lo minimo en lo que se pueden dividir (media)
-        if(b>a && c>a){ //Se necesita conocer el numero menor para saber en cuanto se puede dividir //8>4>6
-            numeromenor=a; //Si a es mayor que b, b es el numero menor //FALSO
-        }else if(a>b && c>b){
-            numeromenor=b; //Sino el numero menor es a //numeromenor=8
+        if(b<a && c<a){ //Se necesita conocer el numero menor para saber en cuanto se puede dividir //8>4>6
+            numeromaximo=a; //Si a es mayor que b, b es el numero menor //FALSO
+        }else if(a<b && c<b){
+            numeromaximo=b; //Sino el numero menor es a //numeromenor=8
         }else{
-            numeromenor=c;
+            numeromaximo=c;
         }
-        while(numeromenor>=divisor){ //Mientras que el numero menor sea mayor o igual al divisor, //8>=2 VERDADERO
+        while(numeromaximo>=divisor){ //Mientras que el numero menor sea mayor o igual al divisor, //8>=2 VERDADERO
             if(a%divisor==0 || b%divisor==0 || c%divisor==0){ //Si el residuo de la division de a entre el divisor es 0 //2%2==0 && 8%2==0
                                               //y si el residuo de la division de b entre el divisor es 0 //VERDADERO && VERDADERO = VERDADERO
+                d=d*divisor;
                 if(a%divisor==0){
                     a=a/divisor;
                 }
@@ -58,13 +59,12 @@ public class PP2Problema18 {
                 if(c%divisor==0){
                     c=c/divisor;
                 }
-                d=d*divisor;//c sera igual a la multiplicacion de c por le divisor //c=1*2 c=2
             }else{ //Sino se cumple el if a divisor se le sumara 1
                 divisor++;
             }
         }
-        //Se imprime el maximo comun denominador //c=2
-        System.out.print("El maximo comun denominador es: " + d);
+        //Se imprime el minimo comun multiplo //
+        System.out.print("El minimo comun multiplo es: " + d);
     }
     
 }
